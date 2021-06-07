b720121c3845a01195e35bd830809a945c9c5dad
david.crespo <david.crespo@hmcts.net>
2021-06-07 11:55:26 +0100

    add scheduler to run the app every 10 secs add integration test

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmonitor/services/TaskConfiguratorSchedulerTest.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.wataskmonitor.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.wataskmonitor.Application;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest(classes = {Application.class, TaskConfiguratorScheduler.class})
+@ExtendWith(SpringExtension.class)
+class TaskConfiguratorSchedulerTest {
+
+    @SpyBean
+    private TaskConfiguratorScheduler taskConfiguratorScheduler;
+
+    @Test
+    void runTaskConfigurator() {
+        await().atMost(12, TimeUnit.SECONDS)
+            .untilAsserted(
+                () -> verify(taskConfiguratorScheduler, times(2)).runTaskConfigurator());
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/wataskmonitor/services/TaskConfiguratorScheduler.java
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.reform.wataskmonitor.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class TaskConfiguratorScheduler {
+
+    @Scheduled(fixedRate = 10000)
+    public void runTaskConfigurator() {
+        log.info("Task configurator starts...");
+        log.info("Task configurator ends...");
+    }
+}
