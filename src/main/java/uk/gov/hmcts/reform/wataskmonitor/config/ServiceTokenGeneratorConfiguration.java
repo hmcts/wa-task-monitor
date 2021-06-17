9f5ac0b49545dbb7be2ae122c4fb0a03dbaef746
david.crespo <david.crespo@hmcts.net>
2021-06-17 21:47:33 +0100

    Revert "remove service token config annotation"
    This reverts commit 775e99a862a503decdf6eb4084bad95d68a2e86a.

--- src/main/java/uk/gov/hmcts/reform/wataskmonitor/config/ServiceTokenGeneratorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmonitor/config/ServiceTokenGeneratorConfiguration.java
@@ -1,12 +1,18 @@
 package uk.gov.hmcts.reform.wataskmonitor.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
 
+@Configuration
+@Lazy
 public class ServiceTokenGeneratorConfiguration {
 
+    @Bean
     public AuthTokenGenerator authTokenGenerator(
         @Value("${idam.s2s-auth.secret}") String secret,
         @Value("${idam.s2s-auth.name}") String microService,
