8be161a11b064ec2203c43a2ba08ee5e4a316b8a
david.crespo <david.crespo@hmcts.net>
2021-06-17 20:24:03 +0100

    fix s2s issue

--- src/main/java/uk/gov/hmcts/reform/wataskmonitor/config/ServiceTokenGeneratorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmonitor/config/ServiceTokenGeneratorConfiguration.java
@@ -3,11 +3,13 @@ package uk.gov.hmcts.reform.wataskmonitor.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
 
 @Configuration
+@Lazy
 public class ServiceTokenGeneratorConfiguration {
 
     @Bean
