1a252e8614fafb5b14a3a3c438ad754306006f5f
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-06-25 10:08:21 +0100

    Remove system out

--- src/main/java/uk/gov/hmcts/reform/wataskmonitor/schedulers/TaskConfiguratorScheduler.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmonitor/schedulers/TaskConfiguratorScheduler.java
@@ -35,7 +35,6 @@ public class TaskConfiguratorScheduler {
         log.info("Starting task configurator.");
         String serviceToken = authTokenGenerator.generate();
         List<CamundaTask> camundaTasks = camundaService.getUnConfiguredTasks(serviceToken);
-        System.out.println(camundaTasks.size());
         if (!camundaTasks.isEmpty()) {
             taskConfigurationService.configureTasks(camundaTasks, serviceToken);
         }
