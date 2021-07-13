7a5f098de4bf261bffef237976e505e4a6de3d7c
David Crespo <david.crespo@hmcts.net>
2021-07-13 14:35:41 +0100

    fix swagger to allow enter s2s authentication (#6)

--- src/main/java/uk/gov/hmcts/reform/wataskmonitor/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmonitor/config/SwaggerConfiguration.java
@@ -4,10 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import uk.gov.hmcts.reform.wataskmonitor.Application;
+
+import static java.util.Collections.singletonList;
 
 @Configuration
 @EnableSwagger2
@@ -21,6 +26,18 @@ public class SwaggerConfiguration {
             .apis(RequestHandlerSelectors
                 .basePackage(Application.class.getPackage().getName() + ".controllers"))
             .paths(PathSelectors.any())
+            .build()
+            .globalRequestParameters(singletonList(
+                headerServiceAuthorization()
+            ));
+    }
+
+    private RequestParameter headerServiceAuthorization() {
+        return new RequestParameterBuilder()
+            .name("ServiceAuthorization")
+            .description("Keyword `Bearer` followed by a service-to-service token for a whitelisted micro-service")
+            .in(ParameterType.HEADER)
+            .required(true)
             .build();
     }
 
