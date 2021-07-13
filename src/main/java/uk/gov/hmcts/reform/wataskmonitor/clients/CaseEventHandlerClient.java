dd6cb687eeba8ac708db26f0d8208b739c6ad9be
david.crespo <david.crespo@hmcts.net>
2021-07-13 20:52:25 +0100

    add CEH feign client
    add initial logic to the job for testing purpose

---
+++ src/main/java/uk/gov/hmcts/reform/wataskmonitor/clients/CaseEventHandlerClient.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.reform.wataskmonitor.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
+import uk.gov.hmcts.reform.wataskmonitor.models.caseeventhandler.EventInformation;
+
+import javax.validation.Valid;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@FeignClient(
+    name = "caseEventHandler",
+    url = "${case-event-handler.service.url}"
+)
+public interface CaseEventHandlerClient {
+
+    String SERVICE_AUTHORIZATION = "ServiceAuthorization";
+
+    @PostMapping(value = "/messages",
+        consumes = APPLICATION_JSON_VALUE,
+        produces = APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    String sendMessage(@RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorisation,
+                       @Valid @RequestBody EventInformation eventInformation);
+
+}
--- src/main/java/uk/gov/hmcts/reform/wataskmonitor/services/jobs/adhoc/createtasks/CreateTaskJob.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmonitor/services/jobs/adhoc/createtasks/CreateTaskJob.java
@@ -2,14 +2,28 @@ package uk.gov.hmcts.reform.wataskmonitor.services.jobs.adhoc.createtasks;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.wataskmonitor.clients.CaseEventHandlerClient;
+import uk.gov.hmcts.reform.wataskmonitor.models.caseeventhandler.EventInformation;
 import uk.gov.hmcts.reform.wataskmonitor.models.jobs.JobDetailName;
 import uk.gov.hmcts.reform.wataskmonitor.services.jobs.JobService;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 import static uk.gov.hmcts.reform.wataskmonitor.models.jobs.JobDetailName.AD_HOC_CREATE_TASKS;
 
 @Slf4j
 @Component
 public class CreateTaskJob implements JobService {
+
+    private final CaseEventHandlerClient caseEventHandlerClient;
+    private final AuthTokenGenerator authTokenGenerator;
+
+    public CreateTaskJob(CaseEventHandlerClient caseEventHandlerClient, AuthTokenGenerator authTokenGenerator) {
+        this.caseEventHandlerClient = caseEventHandlerClient;
+        this.authTokenGenerator = authTokenGenerator;
+    }
 
     @Override
     @SuppressWarnings("PMD.LawOfDemeter")
@@ -20,6 +34,24 @@ public class CreateTaskJob implements JobService {
     @Override
     public void run(String serviceToken) {
         log.info("Starting '{}'", AD_HOC_CREATE_TASKS);
+
+        //todo: read case Ids
+
+        // make call
+        caseEventHandlerClient.sendMessage(authTokenGenerator.generate(),
+            EventInformation.builder()
+                .eventInstanceId(UUID.randomUUID().toString())
+                .eventTimeStamp(LocalDateTime.now())
+                .caseId("1626204638394494")
+                .jurisdictionId("ia")
+                .caseTypeId("asylum")
+                .eventId("buildCase")
+                .newStateId("caseUnderReview")
+                .userId("some user Id")
+                .build());
+
+        //todo: check task is created successfully
+
         log.info("{} finished successfully.", AD_HOC_CREATE_TASKS);
     }
 
