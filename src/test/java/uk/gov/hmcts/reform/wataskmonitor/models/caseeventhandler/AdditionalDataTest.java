a11a70acc1d8342bde84a7e9509ae1f837adb752
david.crespo <david.crespo@hmcts.net>
2021-07-15 01:04:48 +0100

    add more unit coverage for the LoggingUtility

---
+++ src/test/java/uk/gov/hmcts/reform/wataskmonitor/models/caseeventhandler/AdditionalDataTest.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.reform.wataskmonitor.models.caseeventhandler;
+
+import org.junit.jupiter.api.Test;
+import pl.pojo.tester.api.assertion.Method;
+
+import static pl.pojo.tester.api.assertion.Assertions.assertPojoMethodsFor;
+
+class AdditionalDataTest {
+
+    @Test
+    void isWellImplemented() {
+
+        final Class<?> classUnderTest = AdditionalData.class;
+
+        assertPojoMethodsFor(classUnderTest)
+            .testing(Method.GETTER)
+            .testing(Method.CONSTRUCTOR)
+            .testing(Method.EQUALS)
+            .testing(Method.HASH_CODE)
+            .testing(Method.TO_STRING)
+            .areWellImplemented();
+
+    }
+
+}
--- src/test/java/uk/gov/hmcts/reform/wataskmonitor/services/utilities/LoggingUtilityTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskmonitor/services/utilities/LoggingUtilityTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.wataskmonitor.services.utilities;
 
 import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.wataskmonitor.models.jobs.adhoc.createtasks.CreateTaskJobOutcome;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -9,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 class LoggingUtilityTest {
 
     @Test
-    void logPrettyPrint() {
+    void logPrettyPrintStringArgument() {
         String input = "{\"job_details\" : {\"name\" : \"CONFIGURATION\"}}";
 
         @SuppressWarnings("PMD.LawOfDemeter")
@@ -23,6 +24,28 @@ class LoggingUtilityTest {
 
         assertEquals(expectedOutput, output, "output does not match expected output");
         assertNotEquals(output, input, "output can't be equal to input");
+    }
+
+    @Test
+    void logPrettyPrintObjectArgument() {
+        CreateTaskJobOutcome input = CreateTaskJobOutcome.builder()
+            .taskId("some task id")
+            .processInstanceId("some process instance id")
+            .caseId("someCaseId")
+            .created(true)
+            .build();
+
+        @SuppressWarnings("PMD.LawOfDemeter")
+        String output = LoggingUtility.logPrettyPrint(input);
+
+        String expectedOutput = "{\n"
+                                + "  \"caseId\" : \"someCaseId\",\n"
+                                + "  \"taskId\" : \"some task id\",\n"
+                                + "  \"processInstanceId\" : \"some process instance id\",\n"
+                                + "  \"created\" : true\n"
+                                + "}";
+
+        assertEquals(expectedOutput, output, "output does not match expected output");
     }
 
 }
