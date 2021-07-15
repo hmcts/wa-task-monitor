f461f8421281eeab7d86c62a158e16840ffb6c8a
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-07-15 14:47:02 +0100

    Fix pmd issues

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmonitor/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmonitor/controllers/GetWelcomeTest.java
@@ -2,22 +2,13 @@ package uk.gov.hmcts.reform.wataskmonitor.controllers;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc(addFilters = false)
-public class GetWelcomeTest {
-
-    @Autowired
-    private transient MockMvc mockMvc;
+public class GetWelcomeTest extends SpringBootIntegrationBaseTest {
 
     @DisplayName("Should welcome upon root request with 200 response code")
     @Test
