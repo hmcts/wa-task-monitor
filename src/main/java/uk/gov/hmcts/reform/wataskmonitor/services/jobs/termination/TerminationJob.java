3a2dac799079b3b82057da522a65f62a06afe3ed
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-07-16 15:04:56 +0100

    Remove unused imports

--- src/main/java/uk/gov/hmcts/reform/wataskmonitor/services/jobs/termination/TerminationJob.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmonitor/services/jobs/termination/TerminationJob.java
@@ -4,12 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
-import uk.gov.hmcts.reform.wataskmonitor.domain.camunda.HistoricCamundaTask;
 import uk.gov.hmcts.reform.wataskmonitor.domain.taskmonitor.JobName;
 import uk.gov.hmcts.reform.wataskmonitor.services.jobs.JobService;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static uk.gov.hmcts.reform.wataskmonitor.domain.taskmonitor.JobName.TERMINATION;
 
