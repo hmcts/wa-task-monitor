631b2952bf086decffa06243fff480326c9743f0
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-07-16 15:03:58 +0100

    Remove unused imports

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmonitor/controllers/MonitorTaskJobControllerForTerminationJobTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmonitor/controllers/MonitorTaskJobControllerForTerminationJobTest.java
@@ -20,7 +20,6 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
