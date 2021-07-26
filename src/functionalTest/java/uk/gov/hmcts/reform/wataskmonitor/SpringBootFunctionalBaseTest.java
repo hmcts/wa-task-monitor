2db3c0bf66cce404876121e7ef20b1f75e0f41ea
david.crespo <david.crespo@hmcts.net>
2021-07-26 20:21:21 +0100

    add FTs
    Refactor FTs

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskmonitor/SpringBootFunctionalBaseTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskmonitor/SpringBootFunctionalBaseTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.wataskmonitor;
 
 import io.restassured.RestAssured;
-import lombok.extern.slf4j.Slf4j;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,29 +11,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
-import uk.gov.hmcts.reform.wacaseeventhandler.TestUtility;
-import uk.gov.hmcts.reform.wataskmonitor.domain.taskmonitor.request.JobDetails;
-import uk.gov.hmcts.reform.wataskmonitor.domain.taskmonitor.request.MonitorTaskJobRequest;
 
 import static net.serenitybdd.rest.SerenityRest.given;
-import static org.hamcrest.Matchers.is;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static uk.gov.hmcts.reform.wacaseeventhandler.controllers.MonitorTaskJobControllerUtility.expectedResponse;
-import static uk.gov.hmcts.reform.wataskmonitor.domain.taskmonitor.JobName.AD_HOC_DELETE_PROCESS_INSTANCES;
 
 @SpringBootTest
-@ActiveProfiles({"functional"})
+@ActiveProfiles({"local", "functional"})
 @RunWith(SpringIntegrationSerenityRunner.class)
-@Slf4j
 @SuppressWarnings({"PMD.JUnitTestsShouldIncludeAssert", "PMD.LawOfDemeter"})
-class MonitorTaskJobControllerForAdHocDeleteProcessInstanceTest {
+public class SpringBootFunctionalBaseTest {
 
     @Value("${targets.instance}")
     protected String testUrl;
 
     @Autowired
     private AuthTokenGenerator authTokenGenerator;
-    private String serviceToken;
+    public String serviceToken;
 
     @BeforeEach
     void setUp() {
@@ -42,19 +34,6 @@ class MonitorTaskJobControllerForAdHocDeleteProcessInstanceTest {
         RestAssured.useRelaxedHTTPSValidation();
 
         serviceToken = authTokenGenerator.generate();
-    }
-
-    @Test
-    void givenMonitorTaskJobRequestShouldReturnStatus200AndExpectedResponse() {
-        given()
-            .contentType(APPLICATION_JSON_VALUE)
-            .header("ServiceAuthorization", serviceToken)
-            .body(TestUtility.asJsonString(new MonitorTaskJobRequest(new JobDetails(AD_HOC_DELETE_PROCESS_INSTANCES))))
-            .when()
-            .post("/monitor/tasks/jobs")
-            .then()
-            .statusCode(HttpStatus.OK.value())
-            .body(is(expectedResponse.apply(AD_HOC_DELETE_PROCESS_INSTANCES.name())));
     }
 
     @Test
