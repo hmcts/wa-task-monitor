190075e4ff67328b632c3121e18dba51bf28f686
David Crespo <david.crespo@hmcts.net>
2021-07-28 15:38:31 +0100

    adjust ES query to discard cases in ended and appealTakenOffline states (#15)
    * adjust ES query to discard cases in ended and appealTakenOffline states
    
    add unit test scenario
    
    * fix PMD issues

--- src/test/java/uk/gov/hmcts/reform/wataskmonitor/services/jobs/adhoc/createtasks/ElasticSearchCaseRetrieverServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskmonitor/services/jobs/adhoc/createtasks/ElasticSearchCaseRetrieverServiceTest.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.wataskmonitor.services.jobs.adhoc.createtasks;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -25,47 +27,80 @@ class ElasticSearchCaseRetrieverServiceTest extends UnitBaseTest {
     @InjectMocks
     private ElasticSearchCaseRetrieverService elasticSearchCaseRetrieverService;
 
-    @Test
-    void retrieveCaseList() {
+    @ParameterizedTest
+    @EnumSource(value = ResourceEnum.class,
+        names = {"AD_HOC_CREATE_TASKS_CCD_ELASTIC_SEARCH_QUERY", "AD_HOC_UPDATE_CASE_CCD_ELASTIC_SEARCH_QUERY"})
+    void retrieveCaseList(ResourceEnum resourceEnum) {
         when(systemUserIdamToken.generate()).thenReturn("some user token");
 
-        elasticSearchCaseRetrieverService.retrieveCaseList(
-            new ElasticSearchRetrieverParameter(
-                "some service token",
-                ResourceEnum.AD_HOC_CREATE_TASKS_CCD_ELASTIC_SEARCH_QUERY
-            ));
-
-        String expected = "{\n"
-                          + "  \"query\": {\n"
-                          + "    \"bool\": {\n"
-                          + "      \"must\": [\n"
-                          + "        {\n"
-                          + "          \"match\": {\n"
-                          + "            \"state\": \"caseUnderReview\"\n"
-                          + "          }\n"
-                          + "        }\n"
-                          + "      ],\n"
-                          + "      \"filter\": [\n"
-                          + "        {\n"
-                          + "          \"range\": {\n"
-                          + "            \"last_state_modified_date\": {\n"
-                          + "              \"lte\": \"2021-07-12\",\n"
-                          + "              \"gte\": \"2021-05-13\"\n"
-                          + "            }\n"
-                          + "          }\n"
-                          + "        }\n"
-                          + "      ]\n"
-                          + "    }\n"
-                          + "  },\n"
-                          + "  \"size\": 276\n"
-                          + "}\n";
+        elasticSearchCaseRetrieverService.retrieveCaseList(new ElasticSearchRetrieverParameter(
+            "some service token",
+            resourceEnum
+        ));
 
         Mockito.verify(ccdClient).searchCases(
             eq("some user token"),
             eq("some service token"),
             eq("Asylum"),
-            eq(expected)
+            eq(getExpectation(resourceEnum))
         );
+    }
+
+    @SuppressWarnings("PMD.AvoidDuplicateLiterals")
+    private String getExpectation(ResourceEnum resourceEnum) {
+        switch (resourceEnum) {
+            case AD_HOC_CREATE_TASKS_CCD_ELASTIC_SEARCH_QUERY:
+                return "{\n"
+                       + "  \"query\": {\n"
+                       + "    \"bool\": {\n"
+                       + "      \"must\": [\n"
+                       + "        {\n"
+                       + "          \"match\": {\n"
+                       + "            \"state\": \"caseUnderReview\"\n"
+                       + "          }\n"
+                       + "        }\n"
+                       + "      ],\n"
+                       + "      \"filter\": [\n"
+                       + "        {\n"
+                       + "          \"range\": {\n"
+                       + "            \"last_state_modified_date\": {\n"
+                       + "              \"lte\": \"2021-07-12\",\n"
+                       + "              \"gte\": \"2021-05-13\"\n"
+                       + "            }\n"
+                       + "          }\n"
+                       + "        }\n"
+                       + "      ]\n"
+                       + "    }\n"
+                       + "  },\n"
+                       + "  \"size\": 276\n"
+                       + "}\n";
+            case AD_HOC_UPDATE_CASE_CCD_ELASTIC_SEARCH_QUERY:
+                return "{\n"
+                       + "  \"query\": {\n"
+                       + "    \"bool\": {\n"
+                       + "      \"must_not\": [\n"
+                       + "        {\n"
+                       + "          \"exists\": {\n"
+                       + "            \"field\": \"data.caseManagementCategory\"\n"
+                       + "          }\n"
+                       + "        },\n"
+                       + "        {\n"
+                       + "          \"match\": {\n"
+                       + "            \"state\": \"ended\"\n"
+                       + "          }\n"
+                       + "        },\n"
+                       + "        {\n"
+                       + "          \"match\": {\n"
+                       + "            \"state\": \"appealTakenOffline\"\n"
+                       + "          }\n"
+                       + "        }\n"
+                       + "      ]\n"
+                       + "    }\n"
+                       + "  }\n"
+                       + "}\n";
+            default:
+                return null;
+        }
     }
 
     @Test
