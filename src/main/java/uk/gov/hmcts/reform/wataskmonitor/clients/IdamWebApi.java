5d9b225f8a1da32592c77d469118cbe29a9b3d10
oguz akin <akinserdaroguzhan@gmail.com>
2021-09-17 15:51:29 +0100

    RWA-669 PACT test for Idam enpoints calls

--- src/main/java/uk/gov/hmcts/reform/wataskmonitor/clients/IdamWebApi.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmonitor/clients/IdamWebApi.java
@@ -1,19 +1,12 @@
 package uk.gov.hmcts.reform.wataskmonitor.clients;
 
-import feign.codec.Encoder;
-import feign.form.spring.SpringFormEncoder;
-import org.springframework.beans.factory.ObjectFactory;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.support.SpringEncoder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import uk.gov.hmcts.reform.wataskmonitor.config.SnakeCaseFeignConfiguration;
 import uk.gov.hmcts.reform.wataskmonitor.domain.idam.Token;
 import uk.gov.hmcts.reform.wataskmonitor.domain.idam.UserInfo;
 
@@ -21,12 +14,11 @@ import java.util.Map;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static uk.gov.hmcts.reform.wataskmonitor.clients.IdamWebApi.FeignConfiguration;
 
 @FeignClient(
     name = "idam-web-api",
     url = "${idam.url}",
-    configuration = FeignConfiguration.class
+    configuration = SnakeCaseFeignConfiguration.class
 )
 public interface IdamWebApi {
     @GetMapping(
@@ -42,13 +34,5 @@ public interface IdamWebApi {
         consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE
     )
     Token token(@RequestBody Map<String, ?> form);
-
-    @Configuration
-    class FeignConfiguration {
-        @Bean
-        @Primary
-        public Encoder feignFormEncoder(ObjectFactory<HttpMessageConverters> messageConverters) {
-            return new SpringFormEncoder(new SpringEncoder(messageConverters));
-        }
-    }
+    
 }
