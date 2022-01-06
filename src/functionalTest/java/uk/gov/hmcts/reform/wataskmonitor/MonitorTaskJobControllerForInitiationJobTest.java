119c28fb39ae5b92825e823d973ef3ed4b0f7226
oguz akin <akinserdaroguzhan@gmail.com>
2022-01-06 17:44:07 +0000

    some tests added for delayed tasks

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskmonitor/MonitorTaskJobControllerForInitiationJobTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskmonitor/MonitorTaskJobControllerForInitiationJobTest.java
@@ -12,6 +12,7 @@ import uk.gov.hmcts.reform.wataskmonitor.domain.taskmonitor.request.MonitorTaskJ
 import uk.gov.hmcts.reform.wataskmonitor.entities.TestVariables;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -19,6 +20,7 @@ import static net.serenitybdd.rest.SerenityRest.given;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static uk.gov.hmcts.reform.wataskmonitor.config.SecurityConfiguration.SERVICE_AUTHORIZATION;
 import static uk.gov.hmcts.reform.wataskmonitor.controllers.MonitorTaskJobControllerUtility.expectedResponse;
@@ -62,9 +64,92 @@ public class MonitorTaskJobControllerForInitiationJobTest extends SpringBootFunc
             .statusCode(HttpStatus.OK.value())
             .body(is(expectedResponse.apply(JobName.INITIATION.name())));
 
-        Map<String, CamundaVariable> camundaVariableMap =
+        Map<String, CamundaVariable> camundaVariables =
             common.getTaskVariables(authenticationHeaders, taskVariables.getTaskId());
-        assertEquals("unassigned", camundaVariableMap.get("taskState").getValue());
+
+        assertEquals(taskVariables.getCaseId(), camundaVariables.get("caseId").getValue());
+        assertEquals("unassigned", camundaVariables.get("taskState").getValue());
+
+    }
+
+    @Test
+    public void task_initiation_job_should_not_initiate_delayed_task_and_taskState_should_be_unconfigured() {
+
+        TestVariables taskVariables = common.setupDelayedTaskAndRetrieveIds();
+
+        assertNotNull(taskVariables);
+        assertNotNull(taskVariables.getCaseId());
+        assertNotNull(taskVariables.getTaskId());
+
+        caseIds.add(taskVariables.getCaseId());
+
+        given()
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(SERVICE_AUTHORIZATION, serviceToken)
+            .body(TestUtility.asJsonString(new MonitorTaskJobRequest(new JobDetails(JobName.INITIATION))))
+            .when()
+            .post("/monitor/tasks/jobs")
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .body(is(expectedResponse.apply(JobName.INITIATION.name())));
+
+        Map<String, CamundaVariable> camundaVariables =
+            common.getTaskVariablesFromCamunda(authenticationHeaders, taskVariables.getTaskId());
+
+        String caseId = ((HashMap) (((HashMap) camundaVariables).get("caseId"))).get("value").toString();
+        String actualTaskState = ((HashMap) (((HashMap) camundaVariables).get("taskState"))).get("value").toString();
+        Object actualCftTaskState = ((HashMap) camundaVariables).get("cftTaskState");
+
+        assertEquals(taskVariables.getCaseId(), caseId);
+        assertEquals("unconfigured", actualTaskState);
+        assertNull(actualCftTaskState);
+
+    }
+
+    @Test
+    public void task_initiation_job_should_initiate_default_task_and_not_initiate_delayed_task() {
+        TestVariables defaultTaskVariables = common.setupTaskAndRetrieveIds();
+
+        assertNotNull(defaultTaskVariables);
+        assertNotNull(defaultTaskVariables.getCaseId());
+        assertNotNull(defaultTaskVariables.getTaskId());
+
+        caseIds.add(defaultTaskVariables.getCaseId());
+
+        TestVariables delayedTaskVariables = common.setupDelayedTaskAndRetrieveIds();
+
+        assertNotNull(delayedTaskVariables);
+        assertNotNull(delayedTaskVariables.getCaseId());
+        assertNotNull(delayedTaskVariables.getTaskId());
+
+        caseIds.add(delayedTaskVariables.getCaseId());
+
+        given()
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(SERVICE_AUTHORIZATION, serviceToken)
+            .body(TestUtility.asJsonString(new MonitorTaskJobRequest(new JobDetails(JobName.INITIATION))))
+            .when()
+            .post("/monitor/tasks/jobs")
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .body(is(expectedResponse.apply(JobName.INITIATION.name())));
+
+
+        Map<String, CamundaVariable> defaultTaskCamundaVariables =
+            common.getTaskVariables(authenticationHeaders, defaultTaskVariables.getTaskId());
+
+        Map<String, CamundaVariable> delayedTaskCamundaVariables =
+            common.getTaskVariablesFromCamunda(authenticationHeaders, delayedTaskVariables.getTaskId());
+
+        String actualDefaultTaskTaskState = defaultTaskCamundaVariables.get("taskState").getValue().toString();
+
+        String actualDelayedTaskTaskState = ((HashMap) (((HashMap) delayedTaskCamundaVariables)
+            .get("taskState"))).get("value").toString();
+
+        assertEquals("unconfigured", actualDelayedTaskTaskState);
+
+        assertEquals("unassigned", actualDefaultTaskTaskState);
+
     }
 
 }
