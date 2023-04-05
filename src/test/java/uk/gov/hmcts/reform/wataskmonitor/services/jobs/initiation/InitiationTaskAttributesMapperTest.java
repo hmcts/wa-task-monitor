e927604d23b36746b504087a152a4832e5ac89c2
vasu <vasu.kaveri@hmcts.net>
2023-04-05 17:52:22 +0100

    RWA-2457: Fix PITest or Mutation Tests and added up coverage

--- src/test/java/uk/gov/hmcts/reform/wataskmonitor/services/jobs/initiation/InitiationTaskAttributesMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskmonitor/services/jobs/initiation/InitiationTaskAttributesMapperTest.java
@@ -4,9 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import uk.gov.hmcts.reform.wataskmonitor.UnitBaseTest;
 import uk.gov.hmcts.reform.wataskmonitor.domain.camunda.CamundaTask;
@@ -18,9 +15,9 @@ import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static uk.gov.hmcts.reform.wataskmonitor.domain.camunda.CamundaTime.CAMUNDA_DATA_TIME_FORMATTER;
 import static uk.gov.hmcts.reform.wataskmonitor.services.jobs.initiation.helpers.InitiationHelpers.createMockCamundaVariables;
 import static uk.gov.hmcts.reform.wataskmonitor.services.jobs.initiation.helpers.InitiationHelpers.createMockedCamundaTask;
@@ -31,38 +28,83 @@ class InitiationTaskAttributesMapperTest extends UnitBaseTest {
     @InjectMocks
     InitiationTaskAttributesMapper initiationTaskAttributesMapper;
 
-    public static Stream<Arguments> scenarioProvider() {
-        Map<String, CamundaVariable> camundaVariables = createMockCamundaVariables();
-
-        Map<String, CamundaVariable> camundaVariablesWithNoAutoAssigned = createMockCamundaVariables();
-        camundaVariablesWithNoAutoAssigned.put("autoAssigned", new CamundaVariable("false", "Boolean"));
-
-        return Stream.of(
-            Arguments.of("all vars are present", camundaVariables),
-            Arguments.of("autoAssigned is not present", camundaVariablesWithNoAutoAssigned)
-        );
-    }
-
     @BeforeEach
     void setUp() {
         initiationTaskAttributesMapper = new InitiationTaskAttributesMapper(new ObjectMapper());
     }
 
-    @ParameterizedTest
-    @MethodSource("scenarioProvider")
-    void should_map_task_attributes(String scenarioName, Map<String, CamundaVariable> variables) {
-        log.info(scenarioName);
+    @Test
+    void should_map_task_attributes_from_camunda_variables() {
+        Map<String, CamundaVariable> camundaVariables = createMockCamundaVariables();
         ZonedDateTime createdDate = ZonedDateTime.now();
         ZonedDateTime dueDate = createdDate.plusDays(1);
 
         CamundaTask camundaTask = createMockedCamundaTask(createdDate, dueDate);
 
-        Map<String, Object> actual = initiationTaskAttributesMapper.mapTaskAttributes(camundaTask, variables);
-        Map<String, Object> expected = getExpectedTaskAttributes(createdDate, dueDate,
-                                                                 "someAssignee",
-                                                                 "someCamundaTaskDescription");
+        Map<String, Object> actual = initiationTaskAttributesMapper.mapTaskAttributes(camundaTask, camundaVariables);
 
-        assertThat(actual).isEqualTo(expected);
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.AUTO_ASSIGNED.value()).getValue().toString(), actual.get(CamundaVariableDefinition.AUTO_ASSIGNED.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.HAS_WARNINGS.value()).getValue().toString(), actual.get(CamundaVariableDefinition.HAS_WARNINGS.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.CASE_ID.value()).getValue().toString(), actual.get(CamundaVariableDefinition.CASE_ID.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.CASE_NAME.value()).getValue().toString(), actual.get(CamundaVariableDefinition.CASE_NAME.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.CASE_TYPE_ID.value()).getValue().toString(), actual.get(CamundaVariableDefinition.CASE_TYPE_ID.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.TASK_STATE.value()).getValue().toString(), actual.get(CamundaVariableDefinition.TASK_STATE.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.CFT_TASK_STATE.value()).getValue().toString(), actual.get(CamundaVariableDefinition.CFT_TASK_STATE.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.LOCATION.value()).getValue().toString(), actual.get(CamundaVariableDefinition.LOCATION.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.LOCATION_NAME.value()).getValue().toString(), actual.get(CamundaVariableDefinition.LOCATION_NAME.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.SECURITY_CLASSIFICATION.value()).getValue().toString(), actual.get(CamundaVariableDefinition.SECURITY_CLASSIFICATION.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.TITLE.value()).getValue().toString(), actual.get(CamundaVariableDefinition.TITLE.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.EXECUTION_TYPE.value()).getValue().toString(), actual.get(CamundaVariableDefinition.EXECUTION_TYPE.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.TASK_SYSTEM.value()).getValue().toString(), actual.get(CamundaVariableDefinition.TASK_SYSTEM.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.JURISDICTION.value()).getValue().toString(), actual.get(CamundaVariableDefinition.JURISDICTION.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.REGION.value()).getValue().toString(), actual.get(CamundaVariableDefinition.REGION.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.APPEAL_TYPE.value()).getValue().toString(), actual.get(CamundaVariableDefinition.APPEAL_TYPE.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.CASE_MANAGEMENT_CATEGORY.value()).getValue().toString(), actual.get(CamundaVariableDefinition.CASE_MANAGEMENT_CATEGORY.value()).toString());
+        assertEquals(camundaVariables.get(CamundaVariableDefinition.ROLE_ASSIGNMENT_ID.value()).getValue().toString(), actual.get(CamundaVariableDefinition.ROLE_ASSIGNMENT_ID.value()).toString());
+
+    }
+
+    @Test
+    void should_map_task_attributes_from_camunda_task() {
+        ZonedDateTime createdDate = ZonedDateTime.now();
+        ZonedDateTime dueDate = createdDate.plusDays(1);
+
+        CamundaTask camundaTask = new CamundaTask(
+            "camundaTaskId",
+            "camundaTaskName",
+            "camundaProcessInstanceId",
+            "camundaAssignee",
+            createdDate,
+            dueDate,
+            "camundaTaskDescription",
+            "camundaTaskOwner",
+            "camundaTaskFormKey"
+        );
+
+        Map<String, CamundaVariable> camundaVariables = new HashMap<>();
+        camundaVariables.put(CamundaVariableDefinition.TASK_TYPE.value(), new CamundaVariable("testTaskType", "String"));
+        camundaVariables.put(CamundaVariableDefinition.TASK_NAME.value(), new CamundaVariable("testTask", "String"));
+        camundaVariables.put(CamundaVariableDefinition.ASSIGNEE.value(), new CamundaVariable("testAssignee", "String"));
+        camundaVariables.put(CamundaVariableDefinition.DESCRIPTION.value(), new CamundaVariable("testDescription", "String"));
+        camundaVariables.put(CamundaVariableDefinition.CREATED.value(), new CamundaVariable("20/06/2022", "String"));
+        camundaVariables.put(CamundaVariableDefinition.DUE_DATE.value(), new CamundaVariable("20/07/2022", "String"));
+        camundaVariables.put(CamundaVariableDefinition.PRIORITY_DATE.value(), new CamundaVariable("20/07/2022", "String"));
+
+
+        Map<String, Object> actual = initiationTaskAttributesMapper.mapTaskAttributes(camundaTask, camundaVariables);
+
+        assertEquals(camundaTask.getName(), actual.get(CamundaVariableDefinition.TASK_NAME.value()).toString());
+        assertEquals(camundaTask.getAssignee(), actual.get(CamundaVariableDefinition.ASSIGNEE.value()).toString());
+        assertEquals(CAMUNDA_DATA_TIME_FORMATTER.format(camundaTask.getDue()), actual.get(CamundaVariableDefinition.DUE_DATE.value()).toString());
+        assertEquals(camundaTask.getDescription(), actual.get(CamundaVariableDefinition.DESCRIPTION.value()).toString());
+
+        assertNotEquals(CAMUNDA_DATA_TIME_FORMATTER.format(camundaTask.getCreated()), actual.get(CamundaVariableDefinition.CREATED.value()).toString());
+        assertNotEquals(camundaVariables.get(CamundaVariableDefinition.TASK_NAME.value()).toString(), actual.get(CamundaVariableDefinition.TASK_NAME.value()).toString());
+        assertNotEquals(camundaVariables.get(CamundaVariableDefinition.ASSIGNEE.value()).toString(), actual.get(CamundaVariableDefinition.ASSIGNEE.value()).toString());
+        assertNotEquals(camundaVariables.get(CamundaVariableDefinition.DUE_DATE.value()).toString(), actual.get(CamundaVariableDefinition.DUE_DATE.value()).toString());
+        assertNotEquals(camundaVariables.get(CamundaVariableDefinition.DESCRIPTION.value()).toString(), actual.get(CamundaVariableDefinition.DESCRIPTION.value()).toString());
+
+        assertNull(actual.get(CamundaVariableDefinition.PRIORITY_DATE.value()));
     }
 
     @Test
@@ -104,15 +146,6 @@ class InitiationTaskAttributesMapperTest extends UnitBaseTest {
         Map<String, Object> attributes
             = new HashMap<>(getExpectedBaseTaskAttributes(createdDate, dueDate, assignee, description));
         attributes.put(CamundaVariableDefinition.TASK_TYPE.value(), customTaskType);
-
-        return attributes;
-    }
-
-    private Map<String, Object> getExpectedTaskAttributes(ZonedDateTime createdDate, ZonedDateTime dueDate,
-                                                          String assignee, String description) {
-        Map<String, Object> attributes
-            = new HashMap<>(getExpectedBaseTaskAttributes(createdDate, dueDate, assignee, description));
-        attributes.put(CamundaVariableDefinition.TASK_TYPE.value(), "someTaskType");
 
         return attributes;
     }
